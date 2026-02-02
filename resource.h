23e4880195adc6ce87f102a709003f97aed8d6b4
CrokVok <rusaleev.danila@yandex.ru>
2026-02-02 17:47:06 +0300

    Refix(bags): ispravil otobragenie dialog sborki(pomenal .rc)

--- resource.h
+++ resource.h
@@ -9,27 +9,54 @@
 #define IDD_DIALOG1                     310
 #define IDC_COMBO1                      1000
 #define IDC_COMBO2                      1001
-#define ID_32771                        32771
-#define ID_32772                        32772
+
 #define ID_SBORKA                       32773
 #define ID_PARAMETRS                    32774
-#define ID_32775                        32775
-#define ID_32776                        32776
-#define ID_32777                        32777
-#define ID_32778                        32778
-#define ID_32779                        32779
 #define ID_GAYKA                        32780
 #define ID_SHAYBA                       32781
 #define ID_BOLT_7796                    32782
 #define ID_BOLT_7817                    32783
+
+// ID для Edit контролов и статических текстов
+#define IDC_EDIT_M                      1002
+#define IDC_EDIT_D                      1003
+#define IDC_EDIT_B                      1004
+#define IDC_EDIT_H                      1005
+#define IDC_EDIT_D_PARAM                1006
+#define IDC_EDIT_D1                     1007
+#define IDC_EDIT_D0                     1008
+#define IDC_EDIT_D1_VAL                 1009
+#define IDC_EDIT_MASS                   1010
+#define IDC_EDIT_D2                     1011
+#define IDC_EDIT_N                      1012
+#define IDC_EDIT_D4                     1013
+#define IDC_EDIT_N3                     1014
+#define IDC_EDIT_L                      1015
+#define IDC_EDIT_L1                     1016
+#define IDC_EDIT_L2                     1017
+#define IDC_EDIT_R                      1018
+#define IDC_EDIT_EXEC                   1019
+
+// ID для статических текстов (labels)
+#define IDC_STATIC_M                    1020
+#define IDC_STATIC_D                    1021
+#define IDC_STATIC_B                    1022
+#define IDC_STATIC_H                    1023
+#define IDC_STATIC_D1                   1024
+#define IDC_STATIC_MASS                 1025
+
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_PARAMETRS                    32773
+#define ID_SBORKA                       32774
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        312
-#define _APS_NEXT_COMMAND_VALUE         32784
-#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_CONTROL_VALUE         1026
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
